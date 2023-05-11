cddd0f9d2bf4c2520aed2407d91f150f718be897
Gerardo Diaz <agustingerardo.diaz@gmail.com>
2023-05-11 01:22:25 -0400

    feat: add user information binding to home

--- app/home/webapp/controller/Home.controller.js
+++ app/home/webapp/controller/Home.controller.js
@@ -1,14 +1,30 @@
 sap.ui.define(
-  ['sap/ui/core/mvc/Controller', 'sap/ui/model/odata/v4/ODataModel', 'sap/m/MessageToast'],
+  ['sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel', 'sap/m/MessageToast'],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, ODataModel, MessageToast) {
+  function (Controller, JSONModel, MessageToast) {
     'use strict';
 
     return Controller.extend('home.home.controller.Home', {
-      onInit: async function () {},
+      onInit: async function () {
+        try {
+          const { '@odata.context': context, ...oUser } = await $.get(
+            '/service/products/userInfo()'
+          );
+          console.log(oUser);
+          const oModel = new JSONModel(oUser);
+          console.log(oModel);
+          this.getView().setModel(oModel, 'user');
+        } catch (err) {
+          console.error(err);
+          MessageToast.show(err);
+        }
+      },
       onLogoutPress: async function () {},
+      formatRoles: function (roles) {
+        return roles.join(', ');
+      },
     });
   }
 );
