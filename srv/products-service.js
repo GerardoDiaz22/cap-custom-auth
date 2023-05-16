52c6972cdc07659b5903d3aad3f1f0c60f956964
GerardoDiaz22 <84580004+GerardoDiaz22@users.noreply.github.com>
2023-05-15 23:45:02 -0400

    feat: add READ logic to gastosConsolidados service

--- srv/products-service.js
+++ srv/products-service.js
@@ -4,4 +4,26 @@ module.exports = cds.service.impl(async function () {
     const parseRoles = Object.keys(roles);
     return { id, username, roles: parseRoles, attr };
   });
+
+  this.after('READ', 'gastosConsolidados', (gastosConsolidadosview, req) => {
+    gastosConsolidadosview.forEach((element) => {
+      let fSum = 0;
+      fSum =
+        parseFloat(element.importemes01) +
+        parseFloat(element.importemes02) +
+        parseFloat(element.importemes03) +
+        parseFloat(element.importemes04) +
+        parseFloat(element.importemes05) +
+        parseFloat(element.importemes06) +
+        parseFloat(element.importemes07) +
+        parseFloat(element.importemes08) +
+        parseFloat(element.importemes09) +
+        parseFloat(element.importemes10) +
+        parseFloat(element.importemes11) +
+        parseFloat(element.importemes12);
+      element.importetotal = parseFloat(fSum).toFixed(2);
+    });
+    console.log('gastosConsolidadosview', gastosConsolidadosview[0].codcuentacontable);
+    return gastosConsolidadosview;
+  });
 });
