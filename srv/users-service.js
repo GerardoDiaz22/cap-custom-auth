abff3aa4d0a4f7405feba3dc85d88ca12785461e
GerardoDiaz22 <84580004+GerardoDiaz22@users.noreply.github.com>
2023-06-05 05:50:43 -0400

    feat: add users management CRUD app

--- srv/users-service.js
+++ srv/users-service.js
@@ -1,7 +1,13 @@
+const bcrypt = require('bcrypt');
+
 module.exports = cds.service.impl(async function () {
   this.on('userInfo', (req) => {
     const { id, username, roles, attr } = req.user;
     const parseRoles = Object.keys(roles);
     return { id, username, roles: parseRoles, attr };
+  });
+
+  this.before('UPDATE', 'Users', async (req) => {
+    req.data.password = await bcrypt.hash(req.data.password, 10); // TODO: check if theres a better way to do this
   });
 });
